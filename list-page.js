e8a9afde77c462f463c6a36a417cf449a85cdbe1
NohaAhmed01 <nohaniho88@gmail.com>
2026-01-16 23:24:08 +0200

    adjusted js code, added delete a task and add a task logic with ehancements

--- list-page.js
+++ list-page.js
@@ -1,29 +1,69 @@
 const noTasksAdd = document.querySelector(".add-to-grocery-list-icon");
 const taskList = document.querySelector(".tasks-list");
 const taskItem = taskList.querySelectorAll(".task-item");
-const taskText = taskList.querySelectorAll(".task-item-content span");
-const addNewTask = taskList.querySelector(".next-task-item");
-/* const done = taskList.querySelectorAll(".done-button");
-const checked = taskList.querySelectorAll(".done-button-inner"); */
 
-noTasksAdd.addEventListener("click", function(){
-    taskList.classList.remove("invisible");
-    noTasksAdd.classList.add("invisible");
+//creating the first task
+noTasksAdd.addEventListener("click", function () {
+  taskList.classList.remove("invisible");
+  noTasksAdd.classList.add("invisible");
 });
 
-addNewTask.addEventListener("click", function(){
-    const newClonedTask = taskItem[0].cloneNode(true);
-    newClonedTask.querySelector("span").innerHTML = "";
-    taskList.insertBefore(newClonedTask, taskItem[taskItem.length-1]);
-})
+taskList.addEventListener("click", (event) => {
+  //handling the checkbox
+  const doneBtn = event.target.closest(".done-button");
+  if (doneBtn) {
+    const innerDiv = doneBtn.querySelector(".done-button-inner");
+    //toogle the checkbox
+    if (innerDiv) {
+      innerDiv.classList.toggle("invisible");
+    }
 
-/* done.forEach((btn, index)=>{
-    btn.addEventListener("click",() => {
-       if(checked[index].classList.contains("invisible"))
-            checked[index].classList.remove("invisible"); 
-       else
-            checked[index].classList.add("invisible"); 
+    //toggle the strike through
+    const taskDone = event.target.closest(".task-item");
+    if (taskDone) {
+      const taskText = taskDone.querySelector("span");
+      if (taskText.innerHTML != "" && !innerDiv.classList.contains("invisible"))
+        taskText.style.textDecoration = "line-through";
+      else taskText.style.textDecoration = "none";
+    }
+  }
 
-    })
-    
-}); */
+  //adding a new task
+  const addNewTaskItem = event.target.closest(".task-item");
+  if (addNewTaskItem) {
+    const addIcon = addNewTaskItem.querySelector(".next-task-item");
+    if (addIcon) {
+      const newClonedTask = taskItem[0].cloneNode(true);
+      newClonedTask.querySelector("span").innerHTML = "";
+      newClonedTask
+        .querySelector(".done-button-inner")
+        .classList.add("invisible");
+      newClonedTask.querySelector("span").style.textDecoration = "none";
+      newClonedTask.style.backgroundColor= "#e8e8e859";
+      taskList.insertBefore(newClonedTask, taskItem[taskItem.length - 1]);
+    }
+  }
+
+  //deleting a task
+  const deleteATask = event.target.closest(".delete-button");
+  if (deleteATask) {
+    const tasks = taskList.querySelectorAll(".task-item");
+    deleteATask.parentNode.style.backgroundColor = "#ff4040b3";
+    if (tasks.length <= 2) {     
+      setTimeout(() => {
+        taskList.classList.add("invisible");
+        noTasksAdd.classList.remove("invisible");
+        const taskText = tasks[0].querySelector("span");
+        taskText.innerHTML = "";
+        taskText.style.textDecoration = "none";
+        tasks[0].querySelector(".done-button-inner").classList.add("invisible");
+        tasks[0].style.backgroundColor = "#e8e8e859";
+      }, 1100);
+    } else {
+      setTimeout(() => {
+        deleteATask.parentNode.remove();
+      }, 1100);
+    }
+  }
+});
+
